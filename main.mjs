5c43b31ccbb0befcdc88a593e2b1699e8ec23d06
Rafael Castro <rafael__gc@hotmail.com>
2022-08-14 12:01:18 -0300

    first commit

---
+++ main.mjs
@@ -0,0 +1,7 @@
+import {mostraIdade, mostraCidade, mostraHobby} from "./funcao.js";
+
+console.log(mostraIdade("Rafael", 27))
+console.log(mostraCidade("Rafael","Piracicaba-SP"))
+console.log(mostraHobby("Rafael", "Ler livros de desenvovlimento pessoal"))
+
+
